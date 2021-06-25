d2e43ce91ac3acbcb2fbb571f66efeae26ebda5f
Andre Jia <ajia.117@gmail.com>
2021-06-25 13:03:51 -0500

    added notes about functional pattern and functional-shared pattern; generate data structures playground

---
+++ 03_JS_fundamentals/data_structures.js
@@ -0,0 +1,30 @@
+class Stack {
+
+  constructor() {
+      this.items = [];
+  }
+
+  push(element) {
+      // TODO: implement me!
+  }
+
+  pop() {
+      // TODO: implement me!
+  }
+
+  size() {
+    // TODO: implement me!
+  }
+
+  peek() {
+      // TODO: implement me!
+  }
+
+  isEmpty() {
+      // TODO: implement me!
+  }
+
+  printStack() {
+      // TODO: implement me!
+  }
+}
